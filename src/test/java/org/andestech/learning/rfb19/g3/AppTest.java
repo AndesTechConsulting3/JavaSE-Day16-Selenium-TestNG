6d2dc1e0f455e61bbaa71986d0c673e4753c64ea
a0z9 <a0z9@rambler.ru>
2019-02-28 17:39:49 +0300

    1

---
+++ src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
@@ -0,0 +1,53 @@
+package org.andestech.learning.rfb19.g3;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AppTest
+{
+    private WebDriver wd = null;
+
+    @BeforeClass
+    public void initData(){
+    System.setProperty("webdriver.chrome.driver",
+            "E:\\drivers\\selenium\\chromedriver.exe");
+    System.out.println("+++ Class: " + this);
+
+    }
+
+    @Test
+    public void testCaseChrome01()
+    {
+        wd = new ChromeDriver();
+        wd.get("http://google.com");
+
+
+
+        WebElement element = wd.findElement(By.name("q"));
+        System.out.println(element);
+
+
+        WebElement element2 = wd.findElement(By.name("btnK"));
+        System.out.println(element2);
+
+
+
+        assertTrue( true );
+    }
+
+
+    @AfterClass
+    public void tearDown()
+    {
+      if(wd != null) wd.quit();
+      System.out.println("--- Class: " + this);
+    }
+
+}
